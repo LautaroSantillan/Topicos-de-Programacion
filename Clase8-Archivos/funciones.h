03966d2b970533f0555959c113f74d8eb299249b
Lautaro Ezequiel Santillan <santillanlautaro03@gmail.com>
2023-11-09 21:16:15 -0300

    Add files via upload

--- Clase8-Archivos/funciones.h
+++ Clase8-Archivos/funciones.h
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#define ERROR_ARCH -1
 typedef struct{
     char nombre[50];
     char apellido[50];
--- Clase8-Archivos/main.c
+++ Clase8-Archivos/main.c
@@ -18,7 +18,7 @@ int main()
     fclose(pf);*/
 
     ///Leer un archivo
-    FILE * pf;
+    /*FILE * pf;
     pf=fopen("datos.dat", "rb");
     if(pf==NULL)
     {
@@ -28,7 +28,51 @@ int main()
     formulario persona;
     fread(&persona, sizeof(formulario), 1, pf);
     printf("%s - %s - %d", persona.nombre, persona.apellido, persona.edad);
-    fclose(pf);
+    fclose(pf);*/
+
+    ///FSEEK
+//    FILE *pf = fopen("datos.dat", "r+b");
+//    if(!pf)
+//    {
+//        puts("Error al crear el archivo\n");
+//        return ERROR_ARCH;
+//    }
+////    formulario persona[3]={
+////        {"Lautaro", "Santillan", 19},
+////        {"Gaston", "Gimenez", 38},
+////        {"Marcelo", "Lopez", 49},
+////    };
+////    fwrite(&persona, sizeof(formulario), 3, pf);
+//    formulario persona;
+//    fread(&persona, sizeof(formulario), 1, pf);
+//    while(!feof(pf))
+//    {
+//        if(persona.edad >= 25)
+//        {
+//            persona.edad += 1;
+//            fseek(pf, (long)(-1*(sizeof(formulario))), SEEK_CUR);
+//            fwrite(&persona, sizeof(formulario), 1, pf);
+//        }
+//        fseek(pf, 0L, SEEK_CUR);
+//        printf("%s - %s - %d \n", persona.nombre, persona.apellido, persona.edad);
+//        fread(&persona, sizeof(formulario), 1, pf);
+//    }
+//    fclose(pf);
+
+    ///FTELL
+    FILE *pf = fopen("datos.dat", "r+b");
+    if(!pf)
+    {
+        puts("Error al crear el archivo\n");
+        return ERROR_ARCH;
+    }
+    //fseek(pf, 0L, SEEK_SET);
+    fseek(pf, 0L, SEEK_END);
+    int len = ftell(pf);
+    printf("Tamanio del archivo: %d\n", len);
+
+    int cantRegs = ftell(pf)/sizeof(formulario);
+    printf("Cantidad de registros: %d\n", cantRegs);
 
     return 0;
 }
